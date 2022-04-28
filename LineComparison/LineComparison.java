f8fcaf24ab50b3f8af9910f6ace597327619ed15
HarishKamaras <harishkamarasu@gmail.com>
2022-04-28 23:11:24 +0530

    firstcommit to UC2

--- LineComparison/LineComparison.java
+++ LineComparison/LineComparison.java
@@ -1,11 +1,10 @@
 package com.brigdelabz.LineComparison;
 
+import java.lang.*;
 import java.util.Scanner;
 
 public class LineComparison {
-    public static void main(String[] args)
-
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the first Co-oridnates : ");
@@ -24,9 +23,20 @@ public class LineComparison {
 
         double len1, len2;
 
-        len1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        len2=Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
-        System.out.println("Lenghtbetween first line"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+len1);
-        System.out.println("Lenghtbetween second line"+"("+x3+","+y3+"),"+"("+x4+","+y4+")===>"+len2);
+        len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        len2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Lenghtbetween first line" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + len1);
+        System.out.println("Lenghtbetween second line" + "(" + x3 + "," + y3 + ")," + "(" + x4 + "," + y4 + ")===>" + len2);
+
+        Double d1 = new Double(len1);
+        Double d2 = new Double(len2);
+        if (d1.equals(d2)) {
+
+            System.out.println("len1 equals to len2");
+        } else {
+            System.out.println("len1 not equals to len2");
+
+
+        }
     }
 }
